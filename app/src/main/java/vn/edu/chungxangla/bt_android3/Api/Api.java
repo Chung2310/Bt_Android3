07cbd00092cb85d4adca5f9f9d9b685acdcc5170
Chung2310 <chunga00d@gmail.com>
2025-02-10 21:51:41 +0700

    xoá những phần thừa

--- app/src/main/java/vn/edu/chungxangla/bt_android3/Api/Api.java
+++ app/src/main/java/vn/edu/chungxangla/bt_android3/Api/Api.java
@@ -1,18 +1,13 @@
 package vn.edu.chungxangla.bt_android3.Api;
 
-import io.reactivex.rxjava3.core.Observable;
-
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 import vn.edu.chungxangla.bt_android3.Model.LastIDModel;
-import vn.edu.chungxangla.bt_android3.Model.Message;
 
 public interface Api {
     @GET("android/api.aspx")
     Call<LastIDModel> getLastIdData(@Query("action") String action);
 
-    @GET("android/api.aspx")
-    Observable<Message> getBody(@Query("id") int id);
 
 }
