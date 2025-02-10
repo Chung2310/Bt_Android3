e97e6ced7380d4f1d4e24989fd2c77a4323f25be
Chung2310 <chunga00d@gmail.com>
2025-02-10 19:54:05 +0700

    sửa lại cho đúng đầu bài

--- app/src/main/java/vn/edu/chungxangla/bt_android3/Activity/MainActivity.java
+++ app/src/main/java/vn/edu/chungxangla/bt_android3/Activity/MainActivity.java
@@ -11,55 +11,29 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
-import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.app.NotificationCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.google.gson.Gson;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
-import io.reactivex.rxjava3.disposables.CompositeDisposable;
-import io.reactivex.rxjava3.schedulers.Schedulers;
-import io.reactivex.rxjava3.core.Observable;
-import io.reactivex.rxjava3.schedulers.Schedulers;
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-import vn.edu.chungxangla.bt_android3.Adapter.MessageAdapter;
 import vn.edu.chungxangla.bt_android3.Api.Api;
 import vn.edu.chungxangla.bt_android3.Api.RetrofitClient;
 import vn.edu.chungxangla.bt_android3.Model.LastIDModel;
-import vn.edu.chungxangla.bt_android3.Model.Message;
-import vn.edu.chungxangla.bt_android3.Model.MessageModel;
-import vn.edu.chungxangla.bt_android3.Model.MessagesModel;
 import vn.edu.chungxangla.bt_android3.R;
 
 public class MainActivity extends AppCompatActivity {
-    RecyclerView recyclerView;
-    TextView id_main,title_main,body_main,time_main;
-    AppCompatButton button;
+    TextView id_main,id_moi;
     Api api;
-    List<Message> messageList = new ArrayList<>();
-    MessageAdapter adapter;
-    CompositeDisposable compositeDisposable;
     int last_ID;
     private static final String CHANNEL_ID = "my_channel_id";
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,25 +45,16 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        api = RetrofitClient.getInstance("https://57kmt.duckdns.org/android/").create(Api.class);
+        api = RetrofitClient.getClient().create(Api.class);
         anhXa();
-        listAll();
-        //lastId();
-        SharedPreferences sharedPreferences = getSharedPreferences("mypref",MODE_PRIVATE);
+        lastId();
+        sharedPreferences = getSharedPreferences("mypref",MODE_PRIVATE);
         last_ID = sharedPreferences.getInt("lastid",0);
         Handler handler = new Handler(Looper.getMainLooper());
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                listAll();
-            }
-        });
 
         Runnable periodicTask = new Runnable() {
             @Override
             public void run() {
-                listAll();
                 lastId();
                 handler.postDelayed(this, 30000); // Lặp lại sau 30 giây
             }
@@ -98,84 +63,39 @@ public class MainActivity extends AppCompatActivity {
 
     }
    private void anhXa() {
-        recyclerView = findViewById(R.id.recyclerView);
         id_main = findViewById(R.id.id_main);
-        title_main = findViewById(R.id.title_main);
-        body_main = findViewById(R.id.body_main);
-        time_main = findViewById(R.id.time_main);
-        button = findViewById(R.id.button);
-        compositeDisposable = new CompositeDisposable();
+        id_moi = findViewById(R.id.id_moi);
    }
-    private void listAll() {
-        compositeDisposable.add(api.listAll("list_all")
-                .subscribeOn(Schedulers.io()) // Xử lý trên luồng nền
-                .observeOn(AndroidSchedulers.mainThread()) // Kết quả trên luồng chính
-                .subscribe(
-                        messagesModel -> {
-                            // Xử lý thành công
-                            if (adapter == null) {
-                                messageList = messagesModel.getData();
-                                adapter = new MessageAdapter(this, messageList);
-                                recyclerView.setAdapter(adapter);
-                            } else {
-                                adapter.notifyDataSetChanged();
-                            }
-                        },
-                        throwable -> {
-                            // Xử lý lỗi
-                            Log.d("loi",throwable.getMessage()+"");
-                            Toast.makeText(MainActivity.this, "Error: " + throwable.getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                ));
-
-    }
 
     private void lastId() {
-        compositeDisposable.add(api.lastId("last_id")
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                        lastIDModel -> {
-                            if (last_ID < lastIDModel.getLast_id()) {
-                                last_ID = lastIDModel.getLast_id();
-                                getBody(last_ID);
-                                SharedPreferences sharedPreferences = getSharedPreferences("mypref", MODE_PRIVATE);
-                                SharedPreferences.Editor editor = sharedPreferences.edit();
-                                editor.putInt("lastid", lastIDModel.getLast_id());
-                                editor.apply();
-                            }
-                        },
-                        throwable -> {
-                            Toast.makeText(MainActivity.this, "Error: " + throwable.getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                ));
+        Call<LastIDModel> call = api.getLastIdData("last_id");
+        call.enqueue(new Callback<LastIDModel>() {
+            @Override
+            public void onResponse(Call<LastIDModel> call, Response<LastIDModel> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    LastIDModel myResponse = response.body();
+                    if(last_ID < myResponse.getLast_id()) {
+                        id_main.setText("" + last_ID);
+                        id_moi.setText("" + myResponse.getLast_id());
+
+                        showNotification("last ID moi",myResponse.getLast_id()+"");
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putInt("lastid",myResponse.getLast_id());
+                    }
+                } else {
+                    id_main.setText("Lỗi: Không nhận được dữ liệu");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<LastIDModel> call, Throwable t) {
+                id_main.setText("Lỗi: " + t.getMessage());
+                Log.d("loi",t.getMessage());
+            }
+        });
     }
 
-    private void getBody(int id) {
-        compositeDisposable.add(api.getBody(id)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                        message -> {
-                            id_main.setText(message.getId());
-                            title_main.setText(message.getTitle());
-                            body_main.setText(message.getBody());
-                            time_main.setText(message.getTime());
-                            showNotification(message.getTitle(), message.getBody());
-                        },
-                        throwable -> {
-                            Toast.makeText(MainActivity.this, "Error: " + throwable.getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                ));
-    }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (compositeDisposable != null && !compositeDisposable.isDisposed()) {
-            compositeDisposable.dispose();
-        }
-    }
     private void showNotification(String title, String body) {
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
--- app/src/main/java/vn/edu/chungxangla/bt_android3/Api/Api.java
+++ app/src/main/java/vn/edu/chungxangla/bt_android3/Api/Api.java
@@ -1,26 +1,18 @@
 package vn.edu.chungxangla.bt_android3.Api;
 
-import java.util.List;
 import io.reactivex.rxjava3.core.Observable;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.POST;
-import retrofit2.http.Path;
 import retrofit2.http.Query;
 import vn.edu.chungxangla.bt_android3.Model.LastIDModel;
 import vn.edu.chungxangla.bt_android3.Model.Message;
-import vn.edu.chungxangla.bt_android3.Model.MessageModel;
-import vn.edu.chungxangla.bt_android3.Model.MessagesModel;
 
 public interface Api {
-    @GET("android/api.aspx?action=list_all")
-    Observable<MessagesModel> listAll(@Query("action") String action);
+    @GET("android/api.aspx")
+    Call<LastIDModel> getLastIdData(@Query("action") String action);
 
-    @GET("last_id")
-    Observable<LastIDModel> lastId(@Query("action") String action);
-
-    @GET("get_body/{id}")
-    Observable<Message> getBody(@Path("id") int id);
+    @GET("android/api.aspx")
+    Observable<Message> getBody(@Query("id") int id);
 
 }
